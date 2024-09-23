6efe5748530dc3d7c017c4d5475c2bbe502c8af3
heinriej <heinirej@mail.uc.edu>
2024-09-23 12:11:19 -0400

    Initial (final) commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int evenOrOdd = 37;
+        if ( (evenOrOdd % 2) == 0) {
+            System.out.println(evenOrOdd + " is even");
+        }
+        else {
+            System.out.println(evenOrOdd + " is odd");
+        }
+    }
+}
